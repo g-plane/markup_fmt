d47c72e7c820758309faf4ea5497623262b703b5
Pig Fang <g-plane@hotmail.com>
2023-10-15 13:44:34 +0800

    add more about config

--- markup_fmt/src/config.rs
+++ markup_fmt/src/config.rs
@@ -1,4 +1,69 @@
-#[derive(Clone, Default)]
+//! Types about configuration.
+
+#[cfg(feature = "config_serde")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Default)]
+#[cfg_attr(feature = "config_serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "config_serde", serde(rename_all = "camelCase", default))]
+/// The whole configuration of markup_fmt.
+///
+/// For detail, please refer to [Configuration](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md) on GitHub.
+pub struct FormatOptions {
+    #[cfg_attr(feature = "config_serde", serde(flatten))]
+    pub layout: LayoutOptions,
+    #[cfg_attr(feature = "config_serde", serde(flatten))]
+    pub language: LanguageOptions,
+}
+
+#[derive(Clone, Debug)]
+#[cfg_attr(feature = "config_serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "config_serde", serde(rename_all = "camelCase", default))]
+/// Configuration related to layout, such as indentation or print width.
+pub struct LayoutOptions {
+    /// See [`printWidth`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#printwidth) on GitHub
+    pub print_width: usize,
+    /// See [`useTabs`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#usetabs) on GitHub
+    pub use_tabs: bool,
+    /// See [`indentWidth`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#indentwidth) on GitHub
+    pub indent_width: usize,
+    /// See [`lineBreak`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#linebreak) on GitHub
+    pub line_break: LineBreak,
+}
+
+impl Default for LayoutOptions {
+    fn default() -> Self {
+        Self {
+            print_width: 80,
+            use_tabs: false,
+            indent_width: 2,
+            line_break: LineBreak::Lf,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+#[cfg_attr(feature = "config_serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "config_serde", serde(rename_all = "camelCase"))]
+pub enum LineBreak {
+    #[default]
+    Lf,
+    Crlf,
+}
+
+impl From<LineBreak> for tiny_pretty::LineBreak {
+    fn from(value: LineBreak) -> Self {
+        match value {
+            LineBreak::Lf => tiny_pretty::LineBreak::Lf,
+            LineBreak::Crlf => tiny_pretty::LineBreak::Crlf,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+#[cfg_attr(feature = "config_serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "config_serde", serde(rename_all = "camelCase", default))]
+/// Configuration related to syntax.
 pub struct LanguageOptions {
     pub script_indent: bool,
     pub style_indent: bool,
@@ -7,14 +72,18 @@ pub struct LanguageOptions {
     pub v_on_style: Option<VOnStyle>,
 }
 
-#[derive(Clone, Default)]
+#[derive(Clone, Debug, Default)]
+#[cfg_attr(feature = "config_serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "config_serde", serde(rename_all = "camelCase", default))]
 pub enum VBindStyle {
     #[default]
     Short,
     Long,
 }
 
-#[derive(Clone, Default)]
+#[derive(Clone, Debug, Default)]
+#[cfg_attr(feature = "config_serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "config_serde", serde(rename_all = "camelCase", default))]
 pub enum VOnStyle {
     #[default]
     Short,
