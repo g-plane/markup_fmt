c15c405a79f243c94ea3a58735c3b310a8a226fd
Pig Fang <g-plane@hotmail.com>
2023-10-30 18:24:09 +0800

    add doc comments

--- markup_fmt/src/config.rs
+++ markup_fmt/src/config.rs
@@ -66,63 +66,93 @@ impl From<LineBreak> for tiny_pretty::LineBreak {
 #[cfg_attr(feature = "config_serde", serde(rename_all = "camelCase", default))]
 /// Configuration related to syntax.
 pub struct LanguageOptions {
+    /// See [`quotes`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#quotes) on GitHub
     pub quotes: Quotes,
 
+    /// See [`formatComments`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#formatcomments) on GitHub
     pub format_comments: bool,
 
+    /// See [`scriptIndent`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#scriptindent) on GitHub
     pub script_indent: bool,
     #[cfg_attr(feature = "config_serde", serde(rename = "html.scriptIndent"))]
+    /// See [`scriptIndent`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#scriptindent) on GitHub
     pub html_script_indent: Option<bool>,
     #[cfg_attr(feature = "config_serde", serde(rename = "vue.scriptIndent"))]
+    /// See [`scriptIndent`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#scriptindent) on GitHub
     pub vue_script_indent: Option<bool>,
     #[cfg_attr(feature = "config_serde", serde(rename = "svelte.scriptIndent"))]
+    /// See [`scriptIndent`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#scriptindent) on GitHub
     pub svelte_script_indent: Option<bool>,
 
+    /// See [`styleIndent`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#styleindent) on GitHub
     pub style_indent: bool,
     #[cfg_attr(feature = "config_serde", serde(rename = "html.styleIndent"))]
+    /// See [`styleIndent`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#styleindent) on GitHub
     pub html_style_indent: Option<bool>,
     #[cfg_attr(feature = "config_serde", serde(rename = "vue.styleIndent"))]
+    /// See [`styleIndent`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#styleindent) on GitHub
     pub vue_style_indent: Option<bool>,
     #[cfg_attr(feature = "config_serde", serde(rename = "svelte.styleIndent"))]
+    /// See [`styleIndent`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#styleindent) on GitHub
     pub svelte_style_indent: Option<bool>,
 
+    /// See [`closingBracketSameLine`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#closingbracketsameline) on GitHub
     pub closing_bracket_same_line: bool,
 
+    /// See [`closingTagLineBreakForEmpty`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#closingtaglinebreakforempty) on GitHub
     pub closing_tag_line_break_for_empty: ClosingTagLineBreakForEmpty,
 
+    /// See [`maxAttrsPerLine`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#maxattrsperline) on GitHub
     pub max_attrs_per_line: Option<NonZeroUsize>,
 
     #[cfg_attr(feature = "config_serde", serde(rename = "html.normal.selfClosing"))]
+    /// See [`*.selfClosing`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#selfclosing) on GitHub
     pub html_normal_self_closing: Option<bool>,
     #[cfg_attr(feature = "config_serde", serde(rename = "html.void.selfClosing"))]
+    /// See [`*.selfClosing`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#selfclosing) on GitHub
     pub html_void_self_closing: Option<bool>,
     #[cfg_attr(feature = "config_serde", serde(rename = "component.selfClosing"))]
+    /// See [`*.selfClosing`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#selfclosing) on GitHub
     pub component_self_closing: Option<bool>,
     #[cfg_attr(feature = "config_serde", serde(rename = "svg.selfClosing"))]
+    /// See [`*.selfClosing`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#selfclosing) on GitHub
     pub svg_self_closing: Option<bool>,
     #[cfg_attr(feature = "config_serde", serde(rename = "mathml.selfClosing"))]
+    /// See [`*.selfClosing`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#selfclosing) on GitHub
     pub mathml_self_closing: Option<bool>,
 
+    /// See [`whitespaceSensitivity`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#whitespacesensitivity) on GitHub
     pub whitespace_sensitivity: WhitespaceSensitivity,
     #[cfg_attr(
         feature = "config_serde",
         serde(rename = "component.whitespaceSensitivity")
     )]
+    /// See [`whitespaceSensitivity`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#whitespacesensitivity) on GitHub
     pub component_whitespace_sensitivity: Option<WhitespaceSensitivity>,
 
+    /// See [`vBindStyle`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#vbindstyle) on GitHub
     pub v_bind_style: Option<VBindStyle>,
+    /// See [`vOnStyle`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#vonstyle) on GitHub
     pub v_on_style: Option<VOnStyle>,
+    /// See [`vForDelimiterStyle`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#vfordelimiterstyle) on GitHub
     pub v_for_delimiter_style: Option<VForDelimiterStyle>,
+    /// See [`vSlotStyle`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#vslotstyle) on GitHub
     pub v_slot_style: Option<VSlotStyle>,
     #[cfg_attr(feature = "config_serde", serde(rename = "component.vSlotStyle"))]
+    /// See [`vSlotStyle`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#vslotstyle) on GitHub
     pub component_v_slot_style: Option<VSlotStyle>,
     #[cfg_attr(feature = "config_serde", serde(rename = "default.vSlotStyle"))]
+    /// See [`vSlotStyle`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#vslotstyle) on GitHub
     pub default_v_slot_style: Option<VSlotStyle>,
     #[cfg_attr(feature = "config_serde", serde(rename = "named.vSlotStyle"))]
+    /// See [`vSlotStyle`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#vslotstyle) on GitHub
     pub named_v_slot_style: Option<VSlotStyle>,
 
+    /// See [`strictSvelteAttr`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#strictsvelteattr) on GitHub
     pub strict_svelte_attr: bool,
+    /// See [`svelteAttrShorthand`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#svelteattrshorthand) on GitHub
     pub svelte_attr_shorthand: Option<bool>,
+    /// See [`svelteDirectiveShorthand`](https://github.com/g-plane/markup_fmt/blob/main/docs/config.md#sveltedirectiveshorthand) on GitHub
     pub svelte_directive_shorthand: Option<bool>,
 }
 
