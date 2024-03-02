7877061f5dcd794ee245024563769ff839be5d8d
Pig Fang <g-plane@hotmail.com>
2024-03-02 11:03:06 +0800

    remove JS entry of dprint plugin

--- dprint_plugin/deployment/npm/index.ts
+++
@@ -1,8 +0,0 @@
-import * as path from 'node:path'
-import * as URL from 'node:url'
-
-export function getPath() {
-    return path.join(path.dirname(URL.fileURLToPath(import.meta.url)), 'plugin.wasm')
-}
-
-export type * from './config.js'
