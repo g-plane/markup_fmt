11880be84eb4e07407924ec42f6b20b86dfa3e8b
Pig Fang <g-plane@hotmail.com>
2024-04-27 16:32:29 +0800

    ensure trailing newline in integration tests

--- dprint_plugin/tests/integration.rs
+++ dprint_plugin/tests/integration.rs
@@ -22,6 +22,12 @@ fn integration_with_dprint_ts_snapshot() {
                 .unwrap()
                 .stdout;
 
+            assert!(
+                output.ends_with(&[b'\n']),
+                "formatted output should contain trailing newline: {}",
+                path.display()
+            );
+
             build_settings(path.parent().unwrap().join("dprint_ts")).bind(|| {
                 let name = path.file_name().unwrap().to_str().unwrap();
                 assert_snapshot!(name, String::from_utf8(output).unwrap());
@@ -35,6 +41,13 @@ fn integration_with_biome_snapshot() {
     glob!(
         "integration/**/*.{html,vue,svelte,astro,jinja,njk,vto}",
         |path| {
+            let file_name = path.file_name().and_then(|file_name| file_name.to_str());
+            if let Some("return.astro") = file_name {
+                // dprint-plugin-biome doesn't support top-level return (but Biome supports),
+                // so skip it.
+                return;
+            }
+
             let file = fs::File::open(path).unwrap();
 
             let output = Command::new("../node_modules/.bin/dprint")
@@ -49,6 +62,12 @@ fn integration_with_biome_snapshot() {
                 .output()
                 .unwrap()
                 .stdout;
+
+            assert!(
+                output.ends_with(&[b'\n']),
+                "formatted output should contain trailing newline: {}",
+                path.display()
+            );
 
             build_settings(path.parent().unwrap().join("biome")).bind(|| {
                 let name = path.file_name().unwrap().to_str().unwrap();
