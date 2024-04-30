e03b38ae07e9142ebfb5fd20040365dc42565a42
Pig Fang <g-plane@hotmail.com>
2024-04-30 17:54:27 +0800

    remove impls of `Clone` and `Debug`

--- markup_fmt/src/ast.rs
+++ markup_fmt/src/ast.rs
@@ -1,27 +1,22 @@
-#[derive(Clone, Debug)]
 pub struct AstroAttribute<'s> {
     pub name: Option<&'s str>,
     pub expr: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub struct AstroExpr<'s> {
     pub children: Vec<AstroExprChild<'s>>,
 }
 
-#[derive(Clone, Debug)]
 pub enum AstroExprChild<'s> {
     Script(&'s str),
     Template(Vec<Node<'s>>),
 }
 
-#[derive(Clone, Debug)]
 pub struct AstroFrontMatter<'s> {
     pub raw: &'s str,
 }
 
 #[allow(clippy::enum_variant_names)]
-#[derive(Clone, Debug)]
 pub enum Attribute<'s> {
     AstroAttribute(AstroAttribute<'s>),
     NativeAttribute(NativeAttribute<'s>),
@@ -29,19 +24,16 @@ pub enum Attribute<'s> {
     VueDirective(VueDirective<'s>),
 }
 
-#[derive(Clone, Debug)]
 pub struct Comment<'s> {
     pub raw: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub struct VueDirective<'s> {
     pub name: &'s str,
     pub arg_and_modifiers: Option<&'s str>,
     pub value: Option<&'s str>,
 }
 
-#[derive(Clone, Debug)]
 pub struct Element<'s> {
     pub tag_name: &'s str,
     pub attrs: Vec<Attribute<'s>>,
@@ -51,39 +43,32 @@ pub struct Element<'s> {
     pub void_element: bool,
 }
 
-#[derive(Clone, Debug)]
 pub struct JinjaBlock<'s> {
     pub body: Vec<JinjaTagOrChildren<'s>>,
 }
 
-#[derive(Clone, Debug)]
 pub struct JinjaComment<'s> {
     pub raw: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub struct JinjaInterpolation<'s> {
     pub expr: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub struct JinjaTag<'s> {
     pub content: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub enum JinjaTagOrChildren<'s> {
     Tag(JinjaTag<'s>),
     Children(Vec<Node<'s>>),
 }
 
-#[derive(Clone, Debug)]
 pub struct NativeAttribute<'s> {
     pub name: &'s str,
     pub value: Option<&'s str>,
 }
 
-#[derive(Clone, Debug)]
 pub enum Node<'s> {
     AstroExpr(AstroExpr<'s>),
     AstroFrontMatter(AstroFrontMatter<'s>),
@@ -109,24 +94,20 @@ pub enum Node<'s> {
     VueInterpolation(VueInterpolation<'s>),
 }
 
-#[derive(Clone, Debug)]
 pub struct Root<'s> {
     pub children: Vec<Node<'s>>,
 }
 
-#[derive(Clone, Debug)]
 pub struct SvelteAtTag<'s> {
     pub name: &'s str,
     pub expr: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub struct SvelteAttribute<'s> {
     pub name: Option<&'s str>,
     pub expr: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub struct SvelteAwaitBlock<'s> {
     pub expr: &'s str,
     pub then_binding: Option<&'s str>,
@@ -136,13 +117,11 @@ pub struct SvelteAwaitBlock<'s> {
     pub catch_block: Option<SvelteCatchBlock<'s>>,
 }
 
-#[derive(Clone, Debug)]
 pub struct SvelteCatchBlock<'s> {
     pub binding: Option<&'s str>,
     pub children: Vec<Node<'s>>,
 }
 
-#[derive(Clone, Debug)]
 pub struct SvelteEachBlock<'s> {
     pub expr: &'s str,
     pub binding: &'s str,
@@ -152,13 +131,11 @@ pub struct SvelteEachBlock<'s> {
     pub else_children: Option<Vec<Node<'s>>>,
 }
 
-#[derive(Clone, Debug)]
 pub struct SvelteElseIfBlock<'s> {
     pub expr: &'s str,
     pub children: Vec<Node<'s>>,
 }
 
-#[derive(Clone, Debug)]
 pub struct SvelteIfBlock<'s> {
     pub expr: &'s str,
     pub children: Vec<Node<'s>>,
@@ -166,61 +143,50 @@ pub struct SvelteIfBlock<'s> {
     pub else_children: Option<Vec<Node<'s>>>,
 }
 
-#[derive(Clone, Debug)]
 pub struct SvelteInterpolation<'s> {
     pub expr: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub struct SvelteKeyBlock<'s> {
     pub expr: &'s str,
     pub children: Vec<Node<'s>>,
 }
 
-#[derive(Clone, Debug)]
 pub struct SvelteThenBlock<'s> {
     pub binding: &'s str,
     pub children: Vec<Node<'s>>,
 }
 
-#[derive(Clone, Debug)]
 pub struct TextNode<'s> {
     pub raw: &'s str,
     pub line_breaks: usize,
 }
 
-#[derive(Clone, Debug)]
 pub struct VentoBlock<'s> {
     pub body: Vec<VentoTagOrChildren<'s>>,
 }
 
-#[derive(Clone, Debug)]
 pub struct VentoComment<'s> {
     pub raw: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub struct VentoEval<'s> {
     pub raw: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub struct VentoInterpolation<'s> {
     pub expr: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub struct VentoTag<'s> {
     pub tag: &'s str,
 }
 
-#[derive(Clone, Debug)]
 pub enum VentoTagOrChildren<'s> {
     Tag(VentoTag<'s>),
     Children(Vec<Node<'s>>),
 }
 
-#[derive(Clone, Debug)]
 pub struct VueInterpolation<'s> {
     pub expr: &'s str,
 }
