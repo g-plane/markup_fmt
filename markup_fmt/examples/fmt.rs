a78b9c84842d9e8c46b9581c1e41e36fc68ab085
Pig Fang <g-plane@hotmail.com>
2024-05-11 15:45:33 +0800

    refactor example

--- markup_fmt/examples/fmt.rs
+++ markup_fmt/examples/fmt.rs
@@ -1,14 +1,24 @@
-use markup_fmt::{detect_language, format_text};
-use std::{env, fs};
+use markup_fmt::{config::FormatOptions, detect_language, format_text};
+use std::{convert::Infallible, env, error::Error, fs, io};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let file_path = env::args().nth(1).unwrap();
     let language = detect_language(&file_path).unwrap();
-    let code = fs::read_to_string(file_path).unwrap();
+    let code = fs::read_to_string(file_path)?;
+    let options = match fs::read_to_string("markup_fmt.toml") {
+        Ok(s) => toml::from_str(&s)?,
+        Err(error) => {
+            if error.kind() == io::ErrorKind::NotFound {
+                FormatOptions::default()
+            } else {
+                return Err(Box::new(error));
+            }
+        }
+    };
 
-    let formatted = format_text(&code, language, &Default::default(), |_, code, _| {
-        Ok::<_, ()>(code.into())
-    })
-    .unwrap();
+    let formatted = format_text(&code, language, &options, |_, code, _| {
+        Ok::<_, Infallible>(code.into())
+    })?;
     print!("{formatted}");
+    Ok(())
 }
