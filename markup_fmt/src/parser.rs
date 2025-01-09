b0cccf6433e6c3d34ccb3d4fd0c7e014df005168
Thibaut Decombe <thibaut.decombe@gmail.com>
2025-01-09 23:02:36 +0100

    Treat as a JinjaTag instead of JinjaBlock if it contains a `=` sign

--- markup_fmt/src/parser.rs
+++ markup_fmt/src/parser.rs
@@ -1287,10 +1287,10 @@ impl<'s> Parser<'s> {
                 | "autoescape"
                 | "embed"
                 | "with"
-                | "set"
                 | "trans"
                 | "raw"
-        ) {
+        ) || matches!(tag_name, "set") && !&first_tag.content.contains('=')
+        {
             let mut body = vec![JinjaTagOrChildren::Tag(first_tag)];
 
             loop {
