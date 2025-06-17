71394361b98e5459dc732c998453023a15c25ddd
Pig Fang <g-plane@hotmail.com>
2025-06-17 19:13:45 +0800

    test: add complex type syntax in Svelte each block

--- dprint_plugin/tests/integration/basic.svelte
+++ dprint_plugin/tests/integration/basic.svelte
@@ -19,6 +19,7 @@ function generateHtml(data:unknown) {
 {#each [{value:1},{value:2}] as item,index (item   .  value)}...{/each}
 {#each [{value:1},{value:2},{value:3},{value:4}] as {    value},index (value)}...{/each}
 {#each [1 ,2 ,3]  as  Array < number > as value}{/each}
+{#each [] as Array< {[P  in keyof  T as A|B]:T[ P ] }> as binding }{/each}
 
 {#if    Math.random(  )>0.5}...{:else if Date.now(   )>0    }...{/if}
 
