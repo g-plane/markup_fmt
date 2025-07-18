031c200d11e1cb78bedccc1024a0ee7f66f5613b
Enzo Innocenzi <enzo@innocenzi.dev>
2025-07-18 12:16:04 +0200

    fix(vue-component-case): ignore svg tags

--- markup_fmt/tests/fmt/vue/component-case/case.vue
+++ markup_fmt/tests/fmt/vue/component-case/case.vue
@@ -5,4 +5,13 @@
   <my-component></my-component>
   <My-Component></My-Component>
   <myComponent></myComponent>
+  <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+      <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="100%">
+        <stop offset="0%" style="stop-color:rgb(255,255,0);stop-opacity:1" />
+        <stop offset="100%" style="stop-color:rgb(0,0,255);stop-opacity:1" />
+      </linearGradient>
+    </defs>
+    <rect width="200" height="200" fill="url(#grad1)" />
+  </svg>
 </template>
