75961f167cb9b977f3c4d230c75ebe9c40f39455
Thibaut Decombe <68703331+UnknownPlatypus@users.noreply.github.com>
2026-09-01 03:15:13 +0200

    fix: fix raw of nested Jinja block missing opening tag (#254)

--- markup_fmt/src/parser.rs
+++ markup_fmt/src/parser.rs
@@ -1396,6 +1396,7 @@ impl<'s> Parser<'s> {
                         body.push(JinjaTagOrChildren::Children(children));
                     }
                 }
+                let next_tag_start = self.peek_pos();
                 if let Ok(next_tag) = self.parse_jinja_tag() {
                     let next_tag_name = parse_jinja_tag_name(&next_tag);
                     if next_tag_name
@@ -1409,20 +1410,17 @@ impl<'s> Parser<'s> {
                         && matches!(next_tag_name, "elif" | "elseif" | "else")
                     {
                         body.push(JinjaTagOrChildren::Tag(next_tag));
-                    } else if let Some(JinjaTagOrChildren::Children(nodes)) = body.last_mut() {
-                        nodes.push(
-                            self.with_taken(|parser| {
-                                parser.parse_jinja_tag_or_block(Some(next_tag), children_parser)
-                            })
-                            .map(|(kind, raw)| T::build(kind, raw))?,
-                        );
                     } else {
-                        body.push(JinjaTagOrChildren::Children(vec![
-                            self.with_taken(|parser| {
-                                parser.parse_jinja_tag_or_block(Some(next_tag), children_parser)
-                            })
-                            .map(|(kind, raw)| T::build(kind, raw))?,
-                        ]));
+                        let kind =
+                            self.parse_jinja_tag_or_block(Some(next_tag), children_parser)?;
+                        let node = T::build(kind, unsafe {
+                            self.source.get_unchecked(next_tag_start..self.peek_pos())
+                        });
+                        if let Some(JinjaTagOrChildren::Children(nodes)) = body.last_mut() {
+                            nodes.push(node);
+                        } else {
+                            body.push(JinjaTagOrChildren::Children(vec![node]));
+                        }
                     }
                 } else {
                     break;
